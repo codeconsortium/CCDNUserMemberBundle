d8f439c1bfe6bc1f0f978cea03434b56c87b2840
codeconsortium <reece@codeconsortium.com>
2012-02-04 19:23:39 +0000

    Init

---
+++ Controller/MemberController.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * This file is part of the CCDN MemberBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/> 
+ * 
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNUser\MemberBundle\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+/**
+ * 
+ * @author Reece Fowell <reece@codeconsortium.com> 
+ * @version 1.0
+ */
+class MemberController extends ContainerAware
+{
+
+
+	/**
+	 *
+	 * @access public
+	 * @param int $page 
+	 * @return RedirectResponse|RenderResponse
+	 */
+	public function showAction($page)
+	{
+		if ( ! $this->container->get('security.context')->isGranted('ROLE_USER'))
+		{
+			throw new AccessDeniedException('You do not have access to this section.');
+		}
+				
+		$members_paginated = $this->container->get('user.repository')->findAllPaginated();
+
+		$members_per_page = $this->container->getParameter('ccdn_user_member.members_per_page');
+		$members_paginated->setMaxPerPage($members_per_page);
+		$members_paginated->setCurrentPage($page, false, true);
+		
+		$members = $members_paginated->getCurrentPageResults();
+		
+		return $this->container->get('templating')->renderResponse('CCDNUserMemberBundle:List:list.html.' . $this->getEngine(), array(
+			'user_profile_route' => $this->container->getParameter('ccdn_user_member.user.profile_route'),
+			'pager_route' => 'cc_members_paginated',
+			'pager' => $members_paginated,
+			'members' => $members,
+		));
+	}
+	
+	
+	/**
+	 *
+	 * @access public
+	 * @param int $page, char $alpha
+	 * @return RedirectResponse|RenderResponse
+	 */
+	public function showFilteredAction($page, $alpha)
+	{
+		if ( ! $this->container->get('security.context')->isGranted('ROLE_USER'))
+		{
+			throw new AccessDeniedException('You do not have access to this section.');
+		}
+
+		$members_paginated = $this->container->get('user.repository')->findAllFilteredPaginated($alpha);
+
+		$members_per_page = $this->container->getParameter('ccdn_user_member.members_per_page');
+		$members_paginated->setMaxPerPage($members_per_page);
+		$members_paginated->setCurrentPage($page, false, true);
+		
+		$members = $members_paginated->getCurrentPageResults();
+		
+		return $this->container->get('templating')->renderResponse('CCDNUserMemberBundle:List:list.html.' . $this->getEngine(), array(
+			'user_profile_route' => $this->container->getParameter('ccdn_user_member.user.profile_route'),
+			'pager_route' => 'cc_members_alpha_paginated',
+			'pager' => $members_paginated,
+			'members' => $members,
+		));	
+	}
+	
+	
+	/**
+	 *
+	 * @access protected
+	 * @return string
+	 */
+	protected function getEngine()
+    {
+        return $this->container->getParameter('ccdn_user_member.template.engine');
+    }
+}
+
---
+++ DependencyInjection/CCDNUserMemberExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the CCDN MemberBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/> 
+ * 
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNUser\MemberBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * 
+ * @author Reece Fowell <reece@codeconsortium.com> 
+ * @version 1.0
+ */
+class CCDNUserMemberExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+		$container->setParameter('ccdn_user_member.user.profile_route', $config['user']['profile_route']);
+		$container->setParameter('ccdn_user_member.members_per_page', $config['members_per_page']);
+		
+		$container->setParameter('ccdn_user_member.template.engine', $config['template']['engine']);
+		$container->setParameter('ccdn_user_member.template.theme', $config['template']['theme']);
+    }
+}

895a2cd7222db0f6a0ba4e810d1a311384084f39
Reece Fowell <reece@codeconsortium.com>
2012-10-15 00:22:03 +0100

    Switch to twig globals from previous param twig extension.

--- CCDNUserMemberBundle.php
+++ CCDNUserMemberBundle.php
@@ -22,4 +22,31 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class CCDNUserMemberBundle extends Bundle
 {
+				
+	/**
+	 *
+	 * @access public
+	 */
+	public function boot()
+	{
+		$twig = $this->container->get('twig');	
+		$twig->addGlobal('ccdn_user_member', array(
+			'seo' => array(
+				'title_length' => $this->container->getParameter('ccdn_user_member.seo.title_length'),
+			),
+			'login_route' => $this->container->getParameter('ccdn_user_member.login_route'),
+			'account_route' => $this->container->getParameter('ccdn_user_member.sidebar.account_route'),
+			'profile_route' => $this->container->getParameter('ccdn_user_member.sidebar.profile_route'),
+			'login_route' => $this->container->getParameter('ccdn_user_member.sidebar.login_route'),
+			'registration_route' => $this->container->getParameter('ccdn_user_member.sidebar.registration_route'),
+			'reset_route' => $this->container->getParameter('ccdn_user_member.sidebar.reset_route'),
+			'member' => array(
+				'list' => array(
+					'layout_template' => $this->container->getParameter('ccdn_user_member.member.list.layout_template'),
+					'member_since_datetime_format' => $this->container->getParameter('ccdn_user_member.member.list.member_since_datetime_format'),
+				),
+			),
+		));
+	}
+	
 }
